03699f9492bde9bd16b5b741637329e36aa5da8e
unknown <darrengrafton@yahoo.com>
2021-05-25 16:17:24 -0500

    start bills

--- src/pages/Bills.js
+++ src/pages/Bills.js
@@ -6,17 +6,69 @@ import Seo from "../components/seo"
 const Bills = () => {
   const data = useStaticQuery(BILLS_LIST)
 
+  const session3Url = "https://web2.gov.mb.ca/bills/42-3/index.php"
+  const session2Url = "https://web2.gov.mb.ca/bills/42-2/index.php"
+  const session1Url = "https://web2.gov.mb.ca/bills/42-1/index.php"
+
+  const fourthSessionBills = data.allBillsJson.edges.filter(
+    edge => edge.node.session === "4th"
+  )
+  const thirdSessionBills = data.allBillsJson.edges.filter(
+    edge => edge.node.session === "3rd"
+  )
+
+  const secondSessionBills = data.allBillsJson.edges.filter(
+    edge => edge.node.session === "2nd"
+  )
+
+  const firstSessionBills = data.allBillsJson.edges.filter(
+    edge => edge.node.session === "1st"
+  )
+
   return (
     <Layout pageTitle="Bills by Session">
       <Seo title="Bills by Legislative Session" />
-      <ol>
-        {data.allBillsJson.edges.map(edge => (
-          <li key={edge.node.billKey}>
-            {edge.node.billName}
-            <a href={`/bills/${edge.node.billKey}`}>{edge.node.number}</a>
-          </li>
-        ))}
-      </ol>
+
+      <div>
+        <h2>3rd Session</h2>
+        <a href={session3Url}>Gov Mb Listing</a>
+        <ol>
+          {thirdSessionBills.map(edge => (
+            <li key={edge.node.billKey}>
+              <a href={`/bills/${edge.node.billKey}`}>
+                <span>Bill {edge.node.number} - </span>
+                {edge.node.billName}
+              </a>
+            </li>
+          ))}
+        </ol>
+      </div>
+
+      <div>
+        <h2>2nd Session</h2>
+        <a href={session2Url}>Gov Mb Listing</a>
+        <ol>
+          {secondSessionBills.map(edge => (
+            <li key={edge.node.billKey}>
+              {edge.node.billName}
+              <a href={`/bills/${edge.node.billKey}`}>{edge.node.number}</a>
+            </li>
+          ))}
+        </ol>
+      </div>
+
+      <div>
+        <h2>1st Session</h2>
+        <a href={session1Url}>Gov Mb Listing</a>
+        <ol>
+          {firstSessionBills.map(edge => (
+            <li key={edge.node.billKey}>
+              {edge.node.billName}
+              <a href={`/bills/${edge.node.billKey}`}>{edge.node.number}</a>
+            </li>
+          ))}
+        </ol>
+      </div>
     </Layout>
   )
 }
