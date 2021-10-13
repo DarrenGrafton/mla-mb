e494d50038ac55a3ff0f2b50821e1c961a9928fd
Darren Grafton <darrenmgrafton@gmail.com>
2021-10-13 16:23:41 -0500

    added TailWind

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,6 +7,7 @@
 // You can delete this file if you're not using it
 import React from "react"
 import { CookiesProvider } from "react-cookie"
+import "./src/css/index.css"
 
 export const wrapRootElement = ({ element }) => (
   <CookiesProvider>{element}</CookiesProvider>
